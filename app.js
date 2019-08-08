f4036b73415d6a3cefb60aa4e8b6c191a6a1729a
bicipeng <bicipeng@gmail.com>
2019-08-08 15:50:09 -0400

    SQL ESSENTIALS

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const db = require("./database");
+const {db}=require('./models');
 const views = require("./views");
 const layout = require("./views/layout");
+const models=require('./models');
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
 
 //logging middleware
 app.use(morgan("dev"));
@@ -18,4 +24,23 @@ app.get("/", (req, res, next) => {
   res.send(layout());
 });
 
-app.listen(3000, () => console.log("App listening on Port 3000"));
+// const init=async()=>{
+//  await models.User.sync();
+//  await models.Page.sync();
+
+//  app.listen(3000, () => console.log("App listening on Port 3000"));
+// }
+
+const init =async ()=>{
+  
+  await models.db.sync()
+  app.listen(3000,()=>{
+    console.log("App listening on Port 3000");
+  })
+}
+
+
+
+init();
+
+
