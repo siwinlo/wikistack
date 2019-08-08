f7ac9db6a31248cd96401bbfa563d990dada85cb
Siwin Lo <siwin.lo@gmail.com>
2019-08-08 17:48:23 -0400

    database is updating with new posts from wiki/add

--- models/index.js
+++ models/index.js
@@ -1,40 +1,44 @@
-const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack',{
-    logging:false
-})
+const Sequelize = require("sequelize");
+const db = new Sequelize("postgres://localhost:5432/wikistack", {
+  logging: false
+});
 
 // module.exports = {
- 
+
 // }
 
-const Page = db.define('page', {
-    title: {
-      type: Sequelize.STRING,
-      allowNull:false,
-      defaultValue:'No Title'
-    },
-    slug: {
-      type: Sequelize.STRING,
-      allowNull:false
-    },
-    content: {
-      type: Sequelize.TEXT,
-      allowNull:false
-    },
-    status: {
-      type: Sequelize.ENUM('open', 'closed')
-    }
-  });
-  
-  const User = db.define('user', {
-    name: {
-      type: Sequelize.STRING,
-      allowNull:false
-    },
-    email: {
-      type: Sequelize.STRING,
-      allowNull:false
-    }
-  });
-  
-  module.exports = { db, Page, User };
+const Page = db.define("page", {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: "No Title"
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  status: {
+    type: Sequelize.ENUM("open", "closed")
+  }
+});
+
+Page.beforeValidate(page => {
+  page.slug = page.title.replace(/\s+/g, "_").replace(/\W/g, "");
+});
+
+const User = db.define("user", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = { db, Page, User };
--- routes/wiki.js
+++ routes/wiki.js
@@ -2,13 +2,25 @@ const express = require("express");
 const wikiRouter = express.Router();
 const addPage = require("../views/addPage");
 const main = require("../views/main");
+const { Page } = require("../models");
 
 wikiRouter.get("/", (req, res, next) => {
   res.send(main());
 });
 
-wikiRouter.post("/", (req, res, next) => {
-  res.json(req.body);
+wikiRouter.post("/", async (req, res, next) => {
+  const page = new Page({
+    title: req.body.title,
+    content: req.body.content
+  });
+  try {
+    console.log(page);
+    await page.save();
+    res.redirect("/");
+  } catch (error) {
+    next(error);
+  }
+  //res.json(req.body);
 });
 
 wikiRouter.get("/add", (req, res, next) => {
